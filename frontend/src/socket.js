f756152fdeaf1516e7a1a90178b4dd0086ac0553
zhihw <150119423+zhihw@users.noreply.github.com>
2025-06-10 14:10:49 -0700

    RTT

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,4 +2,5 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || "http://localhost:3001";
 const socket = io(SOCKET_URL, { transports: ["websocket"] });
+window.socket = socket;
 export default socket;
